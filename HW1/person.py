c0ee00dce03f1232a94ccfd735cf864e44146e2e
David Nichols <dnicho26@uncc.edu>
2023-01-23 10:19:20 -0500

    Delete HW1 directory

--- HW1/person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-  def __init__(person, name, age, height):
-    person.name = name
-    person.age = age
-    person.height = height
-
-def method():
-    print("{:} is {:} years old.".format(new_person.name, new_person.age))
